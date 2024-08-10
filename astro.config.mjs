cf0f002dd95271d30c88713a27dc6b0c46738a00
AlexDominguez18 <alejandrodominguez1823@gmail.com>
2024-08-09 22:03:59 -0600

    hf: meta information added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,20 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+const  SERVER_PORT = 4321;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://alexdominguez18.github.io";
+const SCRIPT = process.env.npm_lifecycle_event || "";
+const isBuild =  SCRIPT.includes("astro build");
+
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 export default defineConfig({
-  integrations: [tailwind()]
+  server: { port: SERVER_PORT },
+  integrations: [tailwind()],
+  site: BASE_URL,
 });

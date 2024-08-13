9c2a64bac16e92572ac59d0032ecef2bdc162f89
AlexDominguez18 <alejandrodominguez1823@gmail.com>
2024-08-13 14:10:50 -0600

    feat: tags pill and icons added

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,9 +5,9 @@ export default [
     description: "Developed a ticketing system for managing and tracking issues across multiple platforms.",
     link: "https://udp.itrans.cucei.udg.mx/",
     images: [
-      "/images/projects/assist/1.png",
-      "/images/projects/assist/2.png",
-      "/images/projects/assist/3.png",
+      "/images/projects/assist/1.jpg",
+      "/images/projects/assist/2.jpg",
+      "/images/projects/assist/3.jpg",
     ],
     tags: [
       "laravel",
@@ -23,9 +23,9 @@ export default [
     description: "Developed a chat system for Assist that handles multiple client conversations simultaneously. This versatile chat can be integrated into various systems and includes a simple chatbot to address common questions across each platform",
     link: "https://chat.itrans.cucei.udg.mx/",
     images: [
-      "/images/projects/chat/1.png",
-      "/images/projects/chat/2.png",
-      "/images/projects/chat/3.png",
+      "/images/projects/chat/1.jpg",
+      "/images/projects/chat/2.jpg",
+      "/images/projects/chat/3.jpg",
     ],
     tags: [
       "php",
@@ -40,9 +40,9 @@ export default [
     description: "Developed a comprehensive system for managing courses, student and teacher information for the DGE department at CUCEA. The platform also facilitates payment processing, tracks attendance, and records grades.",
     link: "https://ems.cucea.udg.mx/",
     images: [
-      "/images/projects/ems/1.png",
-      "/images/projects/ems/2.png",
-      "/images/projects/ems/3.png",
+      "/images/projects/ems/1.jpg",
+      "/images/projects/ems/2.jpg",
+      "/images/projects/ems/3.jpg",
     ],
     tags: [
       "laravel",
@@ -51,6 +51,25 @@ export default [
       "docker",
       "javascript",
       "livewire",
+    ],
+  },
+  {
+    id: 4,
+    title: "DESPPO WEB PAGE",
+    description: "Developed a website for DESPPO, a company specializing in services for the livestock industry. The site includes a blog, a contact form, Google Maps integration, and a landing page. It also features a custom admin panel for managing the blog and products.",
+    link: "https://grupodesppo.com.mx/",
+    images: [
+      "/images/projects/desppo/1.jpg",
+      "/images/projects/desppo/2.jpg",
+      "/images/projects/desppo/3.jpg",
+    ],
+    tags: [
+      "laravel",
+      "php",
+      "mysql",
+      "bootstrap",
+      "javascript",
+      "vuejs",
     ],
   }
 ];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,30 @@ export default {
 	],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }
+        }
+      },
+      animation: {
+        typing: "typing 3s steps(12) infinite alternate, blink .7s infinite"
+      }
+		},
 	},
 	plugins: [
 		require('flowbite/plugin')
